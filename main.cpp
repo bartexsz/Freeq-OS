b8de40a89afc438d1434e107b14dd399fe851eaf
bartexsz <bartexsz@gmail.com>
2012-03-07 10:13:50 +0100

    uncommented code which now work

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(struct multiboot *mboot_ptr)
 {
   screen_clear();
   screen_write("Hello World \n");
-  //screen_write("Freeq OS \n");
-  //screen_write("by Bartlomiej Szczesniak(aka bartexsz, szcbawokon, Jurij Popov)");
+  screen_write("Freeq OS \n");
+  screen_write("by Bartlomiej Szczesniak(aka bartexsz, szcbawokon, Jurij Popov)");
   return 0;
 }
